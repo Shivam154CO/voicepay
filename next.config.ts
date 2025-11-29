7e088ee7da456eb0151bbe806e32822f4c578b5b
Shivam Pawar <shivampawar44330j@gmail.com>
2025-11-30 01:04:34 +0530

    first commit

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,11 @@ const nextConfig = {
     removeConsole: process.env.NODE_ENV === 'production',
   },
   poweredByHeader: false,
+  reactStrictMode: true,
+  swcMinify: true,
+  env: {
+    CUSTOM_KEY: process.env.CUSTOM_KEY,
+  },
 }
 
 module.exports = nextConfig
